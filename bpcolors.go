50ff75e331a1a539ffc2b77b95858003705f0c02
Waldir Borba Junior <wborbajr@gmail.com>
2022-12-01 16:01:26 -0300

    Update bpcolors.go

--- bpcolors.go
+++ bpcolors.go
@@ -2,93 +2,93 @@ package bpcolors
 
 //Regular text
 const (
- Blk "\e[0;30m"
- Red "\e[0;31m"
- Grn "\e[0;32m"
- Yel "\e[0;33m"
- Blu "\e[0;34m"
- Mag "\e[0;35m"
- Cyn "\e[0;36m"
- Wht "\e[0;37m"
+ Blk = "\e[0;30m"
+ Red = "\e[0;31m"
+ Grn = "\e[0;32m"
+ Yel = "\e[0;33m"
+ Blu = "\e[0;34m"
+ Mag = "\e[0;35m"
+ Cyn = "\e[0;36m"
+ Wht = "\e[0;37m"
 )
 
 //Regular bold text
 const (
- Bblk "\e[1;30m"
- Bred "\e[1;31m"
- Bgrn "\e[1;32m"
- Byel "\e[1;33m"
- Bblu "\e[1;34m"
- Bmag "\e[1;35m"
- Bcyn "\e[1;36m"
- Bwht "\e[1;37m"
+ Bblk = "\e[1;30m"
+ Bred = "\e[1;31m"
+ Bgrn = "\e[1;32m"
+ Byel = "\e[1;33m"
+ Bblu = "\e[1;34m"
+ Bmag = "\e[1;35m"
+ Bcyn = "\e[1;36m"
+ Bwht = "\e[1;37m"
 )
 
 //Regular underline text
 const(
- Ublk "\e[4;30m"
- Ured "\e[4;31m"
- Ugrn "\e[4;32m"
- Uyel "\e[4;33m"
- Ublu "\e[4;34m"
- Umag "\e[4;35m"
- Ucyn "\e[4;36m"
- Uwht "\e[4;37m"
+ Ublk = "\e[4;30m"
+ Ured = "\e[4;31m"
+ Ugrn = "\e[4;32m"
+ Uyel = "\e[4;33m"
+ Ublu = "\e[4;34m"
+ Umag = "\e[4;35m"
+ Ucyn = "\e[4;36m"
+ Uwht = "\e[4;37m"
 )
 
 //Regular background
 const (
- Blkb "\e[40m"
- Redb "\e[41m"
- Grnb "\e[42m"
- Yelb "\e[43m"
- Blub "\e[44m"
- Magb "\e[45m"
- Cynb "\e[46m"
- Whtb "\e[47m"
+ Blkb = "\e[40m"
+ Redb = "\e[41m"
+ Grnb = "\e[42m"
+ Yelb = "\e[43m"
+ Blub = "\e[44m"
+ Magb = "\e[45m"
+ Cynb = "\e[46m"
+ Whtb = "\e[47m"
 )
 
 //High intensty background 
 const(
- Blkhb "\e[0;100m"
- Redhb "\e[0;101m"
- Grnhb "\e[0;102m"
- Yelhb "\e[0;103m"
- Bluhb "\e[0;104m"
- Maghb "\e[0;105m"
- Cynhb "\e[0;106m"
- Whthb "\e[0;107m"
+ Blkhb = "\e[0;100m"
+ Redhb = "\e[0;101m"
+ Grnhb = "\e[0;102m"
+ Yelhb = "\e[0;103m"
+ Bluhb = "\e[0;104m"
+ Maghb = "\e[0;105m"
+ Cynhb = "\e[0;106m"
+ Whthb = "\e[0;107m"
 )
 
 //High intensty text
 const(
- Hblk "\e[0;90m"
- Hred "\e[0;91m"
- Hgrn "\e[0;92m"
- Hyel "\e[0;93m"
- Hblu "\e[0;94m"
- Hmag "\e[0;95m"
- Hcyn "\e[0;96m"
- Hwht "\e[0;97m"
+ Hblk = "\e[0;90m"
+ Hred = "\e[0;91m"
+ Hgrn = "\e[0;92m"
+ Hyel = "\e[0;93m"
+ Hblu = "\e[0;94m"
+ Hmag = "\e[0;95m"
+ Hcyn = "\e[0;96m"
+ Hwht = "\e[0;97m"
 )
 
 //Bold high intensity text
 const(
- Bhblk "\e[1;90m"
- Bhred "\e[1;91m"
- Bhgrn "\e[1;92m"
- Bhyel "\e[1;93m"
- Bhblu "\e[1;94m"
- Bhmag "\e[1;95m"
- Bhcyn "\e[1;96m"
- Bhwht "\e[1;97m"
+ Bhblk = "\e[1;90m"
+ Bhred = "\e[1;91m"
+ Bhgrn = "\e[1;92m"
+ Bhyel = "\e[1;93m"
+ Bhblu = "\e[1;94m"
+ Bhmag = "\e[1;95m"
+ Bhcyn = "\e[1;96m"
+ Bhwht = "\e[1;97m"
 )
 
 //Reset
 const(
- Reset "\e[0m"
- Creset "\e[0m"
- Color_reset "\e[0m"
+ Reset = "\e[0m"
+ Creset = "\e[0m"
+ Color_reset = "\e[0m"
  )
 
 // ref - https://gist.github.com/Prakasaka/219fe5695beeb4d6311583e79933a009
