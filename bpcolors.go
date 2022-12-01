62e06cd65c61c4de44afa4e57e3e5bc22f28eb8d
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2022-12-01 16:04:40 -0300

    update escape code

--- bpcolors.go
+++ bpcolors.go
@@ -1,95 +1,95 @@
 package bpcolors
 
-//Regular text
+// Regular text
 const (
- Blk = "\e[0;30m"
- Red = "\e[0;31m"
- Grn = "\e[0;32m"
- Yel = "\e[0;33m"
- Blu = "\e[0;34m"
- Mag = "\e[0;35m"
- Cyn = "\e[0;36m"
- Wht = "\e[0;37m"
+	Blk = "\033[0;30m"
+	Red = "\033[0;31m"
+	Grn = "\033[0;32m"
+	Yel = "\033[0;33m"
+	Blu = "\033[0;34m"
+	Mag = "\033[0;35m"
+	Cyn = "\033[0;36m"
+	Wht = "\033[0;37m"
 )
 
-//Regular bold text
+// Regular bold text
 const (
- Bblk = "\e[1;30m"
- Bred = "\e[1;31m"
- Bgrn = "\e[1;32m"
- Byel = "\e[1;33m"
- Bblu = "\e[1;34m"
- Bmag = "\e[1;35m"
- Bcyn = "\e[1;36m"
- Bwht = "\e[1;37m"
+	Bblk = "\033[1;30m"
+	Bred = "\033[1;31m"
+	Bgrn = "\033[1;32m"
+	Byel = "\033[1;33m"
+	Bblu = "\033[1;34m"
+	Bmag = "\033[1;35m"
+	Bcyn = "\033[1;36m"
+	Bwht = "\033[1;37m"
 )
 
-//Regular underline text
-const(
- Ublk = "\e[4;30m"
- Ured = "\e[4;31m"
- Ugrn = "\e[4;32m"
- Uyel = "\e[4;33m"
- Ublu = "\e[4;34m"
- Umag = "\e[4;35m"
- Ucyn = "\e[4;36m"
- Uwht = "\e[4;37m"
-)
-
-//Regular background
+// Regular underline text
 const (
- Blkb = "\e[40m"
- Redb = "\e[41m"
- Grnb = "\e[42m"
- Yelb = "\e[43m"
- Blub = "\e[44m"
- Magb = "\e[45m"
- Cynb = "\e[46m"
- Whtb = "\e[47m"
+	Ublk = "\033[4;30m"
+	Ured = "\033[4;31m"
+	Ugrn = "\033[4;32m"
+	Uyel = "\033[4;33m"
+	Ublu = "\033[4;34m"
+	Umag = "\033[4;35m"
+	Ucyn = "\033[4;36m"
+	Uwht = "\033[4;37m"
 )
 
-//High intensty background 
-const(
- Blkhb = "\e[0;100m"
- Redhb = "\e[0;101m"
- Grnhb = "\e[0;102m"
- Yelhb = "\e[0;103m"
- Bluhb = "\e[0;104m"
- Maghb = "\e[0;105m"
- Cynhb = "\e[0;106m"
- Whthb = "\e[0;107m"
+// Regular background
+const (
+	Blkb = "\033[40m"
+	Redb = "\033[41m"
+	Grnb = "\033[42m"
+	Yelb = "\033[43m"
+	Blub = "\033[44m"
+	Magb = "\033[45m"
+	Cynb = "\033[46m"
+	Whtb = "\033[47m"
 )
 
-//High intensty text
-const(
- Hblk = "\e[0;90m"
- Hred = "\e[0;91m"
- Hgrn = "\e[0;92m"
- Hyel = "\e[0;93m"
- Hblu = "\e[0;94m"
- Hmag = "\e[0;95m"
- Hcyn = "\e[0;96m"
- Hwht = "\e[0;97m"
+// High intensty background
+const (
+	Blkhb = "\033[0;100m"
+	Redhb = "\033[0;101m"
+	Grnhb = "\033[0;102m"
+	Yelhb = "\033[0;103m"
+	Bluhb = "\033[0;104m"
+	Maghb = "\033[0;105m"
+	Cynhb = "\033[0;106m"
+	Whthb = "\033[0;107m"
 )
 
-//Bold high intensity text
-const(
- Bhblk = "\e[1;90m"
- Bhred = "\e[1;91m"
- Bhgrn = "\e[1;92m"
- Bhyel = "\e[1;93m"
- Bhblu = "\e[1;94m"
- Bhmag = "\e[1;95m"
- Bhcyn = "\e[1;96m"
- Bhwht = "\e[1;97m"
+// High intensty text
+const (
+	Hblk = "\033[0;90m"
+	Hred = "\033[0;91m"
+	Hgrn = "\033[0;92m"
+	Hyel = "\033[0;93m"
+	Hblu = "\033[0;94m"
+	Hmag = "\033[0;95m"
+	Hcyn = "\033[0;96m"
+	Hwht = "\033[0;97m"
 )
 
-//Reset
-const(
- Reset = "\e[0m"
- Creset = "\e[0m"
- Color_reset = "\e[0m"
- )
+// Bold high intensity text
+const (
+	Bhblk = "\033[1;90m"
+	Bhred = "\033[1;91m"
+	Bhgrn = "\033[1;92m"
+	Bhyel = "\033[1;93m"
+	Bhblu = "\033[1;94m"
+	Bhmag = "\033[1;95m"
+	Bhcyn = "\033[1;96m"
+	Bhwht = "\033[1;97m"
+)
+
+// Reset
+const (
+	Reset       = "\033[0m"
+	Creset      = "\033[0m"
+	Color_reset = "\033[0m"
+)
 
 // ref - https://gist.github.com/Prakasaka/219fe5695beeb4d6311583e79933a009
 // ref - https://gist.github.com/RabaDabaDoba/145049536f815903c79944599c6f952a
